d468deda37f4a058dc8d6d39e23c8905b1fc5dff
Hu2y <sanghun_@daum.net>
2019-11-20 02:06:43 +0900

    *scaffolding  - vue/cli  - sass 설치(npm install sass-loader node-sass --save-dev)  - css

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app">
+    Let's build an Instagram Clone!
+	</div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="scss" src="./assets/css/app.scss">
+// Styles from stylesheet
+</style>
