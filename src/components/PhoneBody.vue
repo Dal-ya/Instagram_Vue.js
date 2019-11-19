bbba321b80ad978949b5004604ee3a880e3e246a
Hu2y <sanghun_@daum.net>
2019-11-20 03:29:24 +0900

    *components  - vuegram-post : 하트 아이콘 클릭했을때 style, likes 변경

---
+++ src/components/PhoneBody.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="phone-body">
+    <div class="feed">
+      <vuegram-post
+        v-for="post in posts" 
+        :post="post"
+        :key="posts.indexOf(post)"
+      ></vuegram-post>
+    </div>
+  </div>
+</template>
+
+<script>
+import VuegramPost from './VuegramPost'
+
+export default {
+  name: "PhoneBody",
+  props: ['posts','filters'],
+  components: {
+    VuegramPost
+  }
+};
+</script>
+
+<style lang="scss" src="../assets/css/phone-body.scss">
+// Styles from stylesheet
+</style>
